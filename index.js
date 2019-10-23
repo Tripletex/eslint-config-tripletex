a36697a97c007570eb5c74cf39ea4073706f79e7
Tellef Møllerup Åmdal <tellef@tripletex.no>
2019-10-23 17:05:34 +0200

    Group and sort.

--- index.js
+++ index.js
@@ -15,21 +15,29 @@ module.exports = {
 		"import",
 	],
 	"rules": {
-		"no-trailing-spaces": 2,
-		"react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-		"indent": ["error", "tab", { "SwitchCase": 1 }],
-		"react/jsx-indent": [2, "tab"],
-		"no-tabs": 0,
-		"import/no-extraneous-dependencies": 0,
-		"react/jsx-indent-props": [2, "tab"],
-		"react/sort-comp": 0,
-		"react/require-default-props": 0,
 		"comma-dangle": 0,
-		"max-len": ["error", { "code": 160 }],
-		"linebreak-style": 0,
+		"indent": ["error", "tab", {
+			"SwitchCase": 1
+		}],
+		"import/no-extraneous-dependencies": 0,
 		"import/prefer-default-export": 0,
+		"linebreak-style": 0,
+		"max-len": ["error", {
+			"code": 160
+		}],
+		"no-trailing-spaces": 2,
+		"no-tabs": 0,
+
 		"jsx-a11y/no-autofocus": 0,
 		"jsx-a11y/click-events-have-key-events": 0,
 		"jsx-a11y/no-static-element-interactions": 0,
+
+		"react/jsx-filename-extension": [1, {
+			"extensions": [".js", ".jsx"]
+		}],
+		"react/jsx-indent": [2, "tab"],
+		"react/jsx-indent-props": [2, "tab"],
+		"react/require-default-props": 0,
+		"react/sort-comp": 0,
 	}
 };
